c3289fdbff05077f31550cc400bf78162f2d06dd
Jeremy Liang <59520795+Averesoft@users.noreply.github.com>
2023-12-31 00:01:05 -0500

    Add flask back-end
    needs to style login and register boxes

---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'secret'
+db = SQLAlchemy(app)  
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20
+    )],render_kw={"placeholder": "username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20
+    )],render_kw={"placeholder": "password"})
+    
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                "User already exists.")
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20
+    )], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20
+    )], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Login")
+
+
+@app.route('/')
+def home():
+    return render_template('home-page.html')
+
+@app.route('/login', methods=['GET', 'POST'])  
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                print("Logged in!")
+                return redirect(url_for('home'))
+            else: 
+                flash('Username or Password is incorrect.')
+        else:
+            flash('Username or Password is incorrect.')
+                
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login')) 
+
+    return render_template('register.html', form=form)   
+#with app.app_context():
+ #   db.create_all()
+
+@app.route('/about')
+def about():
+    return render_template('/about-page.html')
+
+@app.route('/statistics')
+def statistics():
+    return render_template('/statistics-page.html')
+
+@app.route('/sources')
+def sources():
+    return render_template('/sources-page.html')
+
+@app.route('/tips')
+def tips():
+    return render_template('/tips-page.html')
+
+@app.route('/tracking')
+def tracking():
+    return render_template('/tracking-page.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
+
+    
